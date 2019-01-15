767bd8cfb81dd61583b99a06c6a2c71095b65be0
Brian S Paskin <21156194+bpaskin@users.noreply.github.com>
2019-01-15 14:17:08 -0500

    Added @Disposes method

--- CDI2.0Examples/src/com/ibm/example/cdi/produces/FavoriteDoctor.java
+++ CDI2.0Examples/src/com/ibm/example/cdi/produces/FavoriteDoctor.java
@@ -1,5 +1,6 @@
 package com.ibm.example.cdi.produces;
 
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 
 public class FavoriteDoctor {
@@ -8,5 +9,9 @@ public class FavoriteDoctor {
 	
 	public @Produces @Favorite String getDoctor() {
 		return doctor;
+	}
+	
+	public void regenerate(@Disposes String instance) {
+		System.out.println(instance + " is regenerating");
 	}
 }
