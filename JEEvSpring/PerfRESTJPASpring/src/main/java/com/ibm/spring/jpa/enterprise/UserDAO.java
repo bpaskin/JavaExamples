a4feb21574eb152a0bbe6f657be01a9415deb3c4
Brian S Paskin <21156194+bpaskin@users.noreply.github.com>
2019-03-27 08:20:13 -0400

    Updated to include @Transactional on methods

--- JEEvSpring/PerfRESTJPASpring/src/main/java/com/ibm/spring/jpa/enterprise/UserDAO.java
+++ JEEvSpring/PerfRESTJPASpring/src/main/java/com/ibm/spring/jpa/enterprise/UserDAO.java
@@ -6,12 +6,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ibm.spring.jpa.model.User;
 
 @Repository
-@Transactional(readOnly = false,isolation = Isolation.READ_COMMITTED,timeout = 5000)
+@Transactional(readOnly = false,isolation = Isolation.READ_COMMITTED,timeout = 5000,propagation=Propagation.REQUIRED)
 public interface UserDAO extends JpaRepository<User, Long> {	
+
+	@Transactional(readOnly = true,isolation = Isolation.READ_COMMITTED,timeout = 5000,propagation=Propagation.REQUIRED)
 	public List<User> findUser(@Param("name") String name);
+	
+	@Override
+	@Transactional(readOnly = true,isolation = Isolation.READ_COMMITTED,timeout = 5000,propagation=Propagation.REQUIRED)
+	public List<User> findAll();
+	
+	@Override
+	@Transactional(readOnly = false,isolation = Isolation.READ_COMMITTED,timeout = 5000,propagation=Propagation.REQUIRED)
+	public User save(User entity);
+	
+	@Override
+	@Transactional(readOnly = false,isolation = Isolation.READ_COMMITTED,timeout = 5000,propagation=Propagation.REQUIRED)
+	public void deleteAll();
 }
